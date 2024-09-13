1bb2c0b463f9b5cef46fad55ef1dfff027324d1e
Najib Ibrahim <mnajib@gmail.com>
2024-09-13 21:06:45 +0800

    'nix build' use callCabal2nix

--- flake.nix
+++ flake.nix
@@ -9,24 +9,57 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            (self: super: {
-              #my-haskell-project = super.haskellPackages.callCabal2nix "my-haskell-project" (self.flake) {};
-              my-haskell-project = super.haskellPackages.callCabal2nix "my-haskell-project" ./app {};
-            })
-          ];
+          #overlays = [
+          #  (self: super: {
+          #    #my-haskell-project = super.haskellPackages.callCabal2nix "my-haskell-project" (self.flake) {};
+          #    my-haskell-project = super.haskellPackages.callCabal2nix "my-haskell-project" ./app {};
+          #  })
+          #];
         };
+
+        # Define a custom derivation
+        #myHaskellProject = pkgs.haskellPackages.mkDerivation {
+        #  pname = "my-haskell-project";
+        #  version = "0.1.0.0";
+        #  src = ./app;
+        #
+        #  # Add the required license field
+        #  license = pkgs.lib.licenses.mit;
+        #
+        #  # Overriding the build phase to use cabal build
+        #  buildPhase = ''
+        #    cabal configure
+        #    cabal build
+        #  '';
+        #
+        #  installPhase = ''
+        #    mkdir -p $out/bin
+        #    cp -r dist-newstyle/build/*/*/*/build/my-haskell-project/my-haskell-project $out/bin/
+        #  '';
+        #};
+
+        # Define the Haskell project using callCabal2nix
+        #myHaskellProject = pkgs.haskellPackages.callCabal2nix "my-haskell-project" ./app {};
+        myHaskellProject = pkgs.haskellPackages.callCabal2nix "my-haskell-project" ./. {};
+
       in
       {
+
         devShell = pkgs.mkShell {
           buildInputs = [
             pkgs.haskellPackages.ghc
             pkgs.haskellPackages.cabal-install
           ];
         };
-        defaultPackage = pkgs.my-haskell-project;
+
+        # when we build with 'nix build'
+        #defaultPackage = pkgs.my-haskell-project;
+        defaultPackage = myHaskellProject;
+        # and then run it with 'nix run'
+
       }
     );
 }
