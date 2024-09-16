533a60179debad6e5a2bbd82bfe7c7efd73d404b
Najib Ibrahim <mnajib@gmail.com>
2024-09-16 21:53:20 +0800

    Speed.hs now include value and unit for both distance and time

--- app/Speed.hs
+++ app/Speed.hs
@@ -2,13 +2,35 @@
 module Main where
 
 import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import Data.List (words)
 
+-- Function to print help message
+printHelp :: IO ()
+printHelp = putStrLn "Usage: speed <distance> <time>\n\
+\Calculate speed given distance and time.\n\
+\Arguments:\n\
+\  <distance>  Distance traveled\n\
+\  <time>      Time taken to travel the distance"
+
+-- Function to handle incorrect input
+handleError :: IO ()
+handleError = putStrLn "Error: Invalid input.\n" >> printHelp
+
+-- Function that processes speed data
+speed :: (Double, String) -> (Double, String) -> IO ()
+speed distance time = do
+  let
+    (distanceValue, distanceUnit) = distance
+    (timeValue, timeUnit) = time
+  putStrLn $ "Distance: " ++ show distanceValue ++ " " ++ distanceUnit ++
+             ", Time: " ++ show timeValue ++ " " ++ timeUnit
+  putStrLn $ "Speed: " ++ show (distanceValue / timeValue)
+
+-- Main app function
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        [distance, time] -> do
-            let dist = read distance :: Double
-                t = read time :: Double
-            putStrLn $ "Speed: " ++ show (dist / t) ++ " units per time"
-        _ -> putStrLn "Usage: speed <distance> <time>"
+  let
+    distance = (60.0, "meter")
+    time = (120, "saat" )
+  speed distance time
